49c292e3de093daffb90b99a1f162f35d8cd86d2
Bastian <powdev.tech@gmail.com>
2025-05-14 19:39:17 -0400

    Cinepedia_devtalles_BottonNavigationBar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -13,7 +13,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return Scaffold(body: Center(child: Text(Environment.theMovieDbKey)));
-    return const Scaffold(body: _HomeView());
+    return const Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottonNavigation(),
+    );
   }
 }
 
